97480f058d9db814fc7188b8d30baf1d149bb4c1
Jeremy <boiselet.jeremy@gmail.com>
2017-12-03 21:14:51 +0100

    big faute

--- View/index.php
+++ View/index.php
@@ -13,7 +13,7 @@ require_once('../Models/cirModel.php');
             Bienvenu sur l'ENT des notes !
           </div>
           <div class="">
-            Ici tu peux enregistrer tes notes pour calculer automatique ta moyenne !<br><br>
+            Ici tu peux enregistrer tes notes pour calculer automatiquement ta moyenne !<br><br>
             <img class="img-resp" src="../Ressources/image/student-life.jpg" alt="Fond d'écran">
           </div>
         </article>
