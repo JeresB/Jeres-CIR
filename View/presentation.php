d9e1009a06aaa8e1347e3ee07bfb15c43cf1475c
Jeremy <boiselet.jeremy@gmail.com>
2017-12-10 12:50:59 +0100

    add fonctionnalités en dev

--- View/presentation.php
+++ View/presentation.php
@@ -10,6 +10,19 @@ require_once('../Models/cirModel.php');
       <main class="ui container">
         <h1 class="ui center dividing aligned header"><i class="wikipedia icon"></i> Présentation</h1>
         <article class="ui center aligned piled segment">
+          <h4 class="ui horizontal divider header">
+            <i class="code icon"></i>
+            Fonctionnalités en développement
+          </h4>
+          <ul class="ui list">
+            <li>Moyenne par matière pour chaque promo.</li>
+            <li>Moyenne par sous modules</li>
+            <li>Présentation - Comment utiliser le site ?</li>
+          </ul>
+          <h4 class="ui horizontal divider header">
+            <i class="tasks icon"></i>
+            Présentation
+          </h4>
           <p>En développement ;)</p>
         </article>
       </main>
