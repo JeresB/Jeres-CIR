0b8a87a21779cf0ceeb9bd878bc2e6728e51da53
Jeremy <boiselet.jeremy@gmail.com>
2017-12-20 20:13:44 +0100

    correction de fautes page aide.php

--- View/aide.php
+++ View/aide.php
@@ -14,18 +14,18 @@ require_once('../Models/cirModel.php');
             <i class="graduation icon"></i>
             Ajouter des matieres pour votre promo
           </h4>
-          <p>Ne connaissant pas la totalité des matières de chaques promos ainsi que leur coefficient, je ne peux pas completer la base de données sans votre aide.
-            <br>N'hésiter pas à m'envoyer la liste de vos matières avec leurs coefficient et le nom de votre promo.</p>
+          <p>Ne connaissant pas la totalité des matières de chaque promo ainsi que leur coefficient, je ne peux pas compléter la base de données sans votre aide.
+            <br>N'hésitez pas à m'envoyer la liste de vos matières avec leurs coefficients et le nom de votre promo.</p>
           <h4 class="ui horizontal divider header">
             <i class="bug icon"></i>
             Déclarer un bug
           </h4>
-          <p>Si vous constater un bug ou un défaut dans le site n'hésiter pas à me le rapporter.</p>
+          <p>Si vous constatez un bug ou un défaut dans le site n'hésitez pas à me le rapporter.</p>
           <h4 class="ui horizontal divider header">
             <i class="line chart icon"></i>
             Proposer une amélioration
           </h4>
-          <p>Si une amélioration vous viens à l'esprit envoyer moi un message pour aider à améliorer ce site.</p>
+          <p>Si une amélioration vous vient à l'esprit envoyez moi un message pour aider à améliorer ce site.</p>
           <h4 class="ui horizontal divider header">
             <i class="mail icon"></i>
             Me contacter
